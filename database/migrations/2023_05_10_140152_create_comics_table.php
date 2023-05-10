56a203d47dfccbbddeed3be351fcc9507bcfd393
P4anix <pancok_1@yahoo.it>
2023-05-10 17:13:17 +0200

    seeder done

--- database/migrations/2023_05_10_140152_create_comics_table.php
+++ database/migrations/2023_05_10_140152_create_comics_table.php
@@ -23,8 +23,8 @@ return new class extends Migration
             $table->string('series', 50);
             $table->date('sale_date');
             $table->string('type', 100);
-            $table->string('artists', 100); 
-            $table->string('writers', 100); 
+            $table->string('artists', 300); 
+            $table->string('writers', 300); 
 
             $table->timestamps();
         });
--- database/seeders/ComicTableSeeder.php
+++ database/seeders/ComicTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comic;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,32 @@ class ComicTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $comics = config("comics");
+        foreach ($comics as $comic) {
+            $newComic = new Comic();
+
+            $newComic->title = $comic['title'];
+            $newComic->description = $comic['description'];
+            $newComic->thumb = $comic['thumb'];
+            $newComic->price = $comic['price'];
+            $newComic->series = $comic['series'];
+            $newComic->sale_date = $comic['sale_date'];
+            $newComic->type = $comic['type'];
+            $newComic->artists = implode(',', $comic['artists']);
+            $newComic->writers = implode(',', $comic['writers']);
+
+            $newComic->save();
+        }
     }
 }
+
+
+        /*  $table->string('title', 100);
+            $table->text('description');
+            $table->text('thumb');
+            $table->float('price', 6, 2);
+            $table->string('series', 50);
+            $table->date('sale_date');
+            $table->string('type', 100);
+            $table->string('artists', 100); 
+            $table->string('writers', 100);  */
